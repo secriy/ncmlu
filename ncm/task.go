c838de22afbeb9d1c075c478306c02a945295f09
Secriy <secriyal@gmail.com>
2021-09-06 18:25:47 +0800

    feat: custom playlists

--- ncm/task.go
+++ ncm/task.go
@@ -6,6 +6,7 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/secriy/ncmlu/config"
 	"github.com/secriy/ncmlu/util"
 )
 
@@ -31,22 +32,32 @@ func NcmluTask(phone, passwd string, code int, play bool) {
 	acc.Sign(client, 1) // PC/Web 端
 
 	if play {
-		// 获取推荐歌单
-		acc.PersonalizedList(client)
-		if acc.PlayList == nil || len(acc.PlayList) == 0 {
-			return
-		}
-		// 获取全部歌曲
-		for _, v := range acc.PlayList {
-			acc.Musics(client, v)
-		}
-		if acc.MusicList == nil || len(acc.MusicList) == 0 {
-			return
-		}
-		// 限制歌曲数量
-		if len(acc.MusicList) > limit {
-			randomMusics(acc.MusicList, limit)
-			acc.MusicList = acc.MusicList[:limit]
+		// 获取歌单
+		if config.Conf.Playlist == nil || len(config.Conf.Playlist) == 0 {
+			// 获取个性推荐推荐歌单
+			acc.PersonalizedList(client)
+			// 获取全部歌曲
+			for _, v := range acc.PlayList {
+				acc.Musics(client, v)
+			}
+			if acc.MusicList == nil || len(acc.MusicList) == 0 {
+				return
+			}
+			// 限制歌曲数量
+			if len(acc.MusicList) > limit {
+				randomMusics(acc.MusicList, limit)
+				acc.MusicList = acc.MusicList[:limit]
+			}
+		} else {
+			// 获取自定义歌单
+			acc.PlayList = config.Conf.Playlist
+			// 获取全部歌曲
+			for _, v := range acc.PlayList {
+				acc.Musics(client, v)
+			}
+			if acc.MusicList == nil || len(acc.MusicList) == 0 {
+				return
+			}
 		}
 		// 刷歌
 		acc.Feedback(client)
